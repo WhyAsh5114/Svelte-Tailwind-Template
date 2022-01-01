21347ce9247dcbf889ca66a070230f52c2cdbf08
WhyAsh5114 <whyashplaysdota@gmail.com>
2022-01-01 15:41:47 +0530

    initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,5 @@
+<div class="flex justify-center">
+    <div class="flex justify-center bg-purple-500 w-full">
+        <h1 class="my-5 font-semibold text-3xl text-white border-2 px-2 border-black">Starter Template</h1>
+    </div>
+</div>
---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./src/**/*.svelte', './src/**/*.css'],
+  darkMode: true,
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
